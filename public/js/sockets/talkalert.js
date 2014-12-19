ab887c63b709762ccf1038b59005f94ca77e098c
elderbas <elderbas@gmail.com>
2014-12-19 16:40:34 -0700

    updated talk alert

--- public/js/sockets/talkalert.js
+++ public/js/sockets/talkalert.js
@@ -4,8 +4,7 @@ io = io.connect();
 io.emit('login', {name:'BMoney'});
 
 io.on('successfulLogin', function(data){
-    clientID = data.clientID;
-})
+    clientID = data.clientID})
 
 // Listen for the talk event.
 io.on('talk', function(data) {
