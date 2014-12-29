abff5bc4af241b76c7e1cd864c35ba77ca5b66cd
Dave Mun <mun.dave@gmail.com>
2014-12-29 13:49:00 -0800

    Other clients have their appropriate webRTC vid rendered onto faces.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-function clog(v){console.log(v);}
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
--- public/js/webRTCComponents/videoStream.js
+++ public/js/webRTCComponents/videoStream.js
@@ -45,7 +45,82 @@ var webrtc = new SimpleWebRTC({
   autoRequestMedia: true
 });
 
+
+
+
+
+var renderOtherCube = function(divID, clientID){
+  var video = document.getElementById(divID);
+   var position =  {
+      xPosition: 0,
+      yPosition: 25.1,
+      zPosition: 0,
+      xRotation: 0,
+      yRotation: 0,
+      zRotation: 0
+    };
+  var debugCube = false;
+  var videoImage = document.getElementById( 'localVideo' );
+
+  var videoTexture = new THREE.VideoTexture( video );
+  videoTexture.generateMipmaps = false;
+  videoTexture.minFilter = THREE.LinearFilter;
+  videoTexture.magFilter = THREE.LinearFilter;
+
+ var materialArray = [];
+  scene = scene || window.scene;
+  materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/xpos.png' ) }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/xneg.png' ) }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/ypos.png' ) }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/yneg.png' ) }));
+  materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/zpos.png' ) }));
+  if(debugCube){
+    materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/zneg.png' ) }));
+  }else{
+    materialArray.push(new THREE.MeshBasicMaterial( { map: videoTexture }));
+  }
+  var MovingCubeMat = new THREE.MeshFaceMaterial(materialArray);
+
+  var cube  = scene.getObjectByName('videoCube'+clientID);
+  cube.material = MovingCubeMat;
+  cube.material.needsUpdate = true;
+}
+
+
+
+
+
+
+webrtc.on('channelMessage', function (peer, label, data) {
+  if (data.type === 'setClientID') {
+    alert('cID received')
+    window.dave = {
+      otherCID: data.payload,
+      vidID: peer.id+'_video_incoming'
+    };
+    renderOtherCube(peer.id+'_video_incoming', data.payload);
+  }
+});
+
+function videoAdd(video,peer){
+  // do the usual stuff, add the new video
+  // where you want etc...
+  // Now, open the dataChannel
+  var dc = peer.getDataChannel('realTalkClient');
+  // Now send my name to all the peers
+  // Add a small timeout so dataChannel has time to be ready
+  setTimeout(function(){
+    webrtc.sendDirectlyToAll('realTalkClient','setClientID', clientID);
+  }, 3000);
+}
+
+webrtc.on('videoAdded', function(video,peer){
+  videoAdd(video,peer);
+});
+
+
 var renderSelfCube = function(err, roomDescription){
+
   var position =  {
       xPosition: 0,
       yPosition: 25.1,
@@ -97,3 +172,11 @@ var initWebRTC = function(){
     webrtc.joinRoom('realTalkClient', renderSelfCube);
   });
 };
+
+
+// // set volume on video tag for all peers takse a value between 0 and 1
+// SimpleWebRTC.prototype.setVolumeForAll = function (volume) {
+//     this.webrtc.peers.forEach(function (peer) {
+//         if (peer.videoEl) peer.videoEl.volume = volume;
+//     });
+// };
