6774f43a11c1e66d11de123f22e3e8960f55dbcc
Dave Mun <davemun@users.noreply.github.com>
2014-12-31 11:47:25 -0800

    Update connect.js
    Changed to deployment.

--- public/js/sockets/connect.js
+++ public/js/sockets/connect.js
@@ -1,5 +1,5 @@
 //change as appropriate
-var _Envs = {"development": true};
+var _Envs = {"development": false};
 
 var realTalkUrl = _Envs.development ? "127.0.0.1" : 'http://realtalkclient.herokuapp.com';
 
