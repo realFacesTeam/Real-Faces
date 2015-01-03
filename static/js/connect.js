83bb9e273bb991cfa6726910b53bf1833ccc7209
Dave Mun <mun.dave@gmail.com>
2015-01-02 17:48:33 -0800

    Changed to new STUN/TURN servers and url handling for io.connect.

--- static/js/connect.js
+++ static/js/connect.js
@@ -1,4 +1,4 @@
-//change as appropriate
-var _Envs = {"development": false};
+// //change as appropriate
+// var _Envs = {"development": false};
 
-var realTalkUrl = _Envs.development ? "127.0.0.1" : 'http://realtalkclient.herokuapp.com';
+// var realTalkUrl = _Envs.development ? "127.0.0.1" : 'http://realtalkclient.herokuapp.com';
